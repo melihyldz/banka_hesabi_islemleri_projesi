e5621f1bcc8250f3dc1d25fa0d8a6be62811a21b
Melih Yıldız <55960496+melihyldz@users.noreply.github.com>
2021-06-13 19:26:25 +0300

    Add files via upload

---
+++ bank_system.cpp
@@ -0,0 +1,40 @@
+﻿#include "bankClass.h"
+#include <locale.h>
+
+
+// Melih Yıldız
+// 13.05.2021
+// 
+// 
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int key, askForNext=1;
+	bankAccount acc1("", "", "", 0.0F);
+	
+	while (askForNext == 1) {
+		cout << "-------ANASAYFA-------" << endl <<
+		"HESABIM (1)---->" << endl << "PARA ÇEKME (2)---->" 
+		<< endl << "PARA YATIRMA (3)---->" << endl;
+		cin >> key;
+		switch (key)
+		{
+		case 1:
+			acc1.controlMyAccount();
+			break;
+		case 2:
+			acc1.withDraw();
+			acc1.controlMyAccount();
+			break;
+		case 3:
+			acc1.deposit();
+			acc1.controlMyAccount();
+			break;
+		default:
+			break;
+		}
+		cout << "Çıkış ---> 0" << endl << "Anasayfa ---> 1" << endl;
+		cin >> askForNext;
+	}
+	
+}
